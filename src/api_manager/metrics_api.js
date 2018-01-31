c3666d262527ce1440ac8b9cb392606da7468e46
jduthon <duthon.jean@gmail.com>
2018-01-31 16:02:59 +0100

    Fixes issue #52: step problem in metrics requests

--- src/api_manager/metrics_api.js
+++ src/api_manager/metrics_api.js
@@ -10,13 +10,18 @@ import {
 } from '../router';
 
 import { dataEvents, D_METRICS } from '../event_hub';
+import config from '../app_config';
 
 import { getMetricsSummaryObs, getMetricsSpeed } from './metrics_agent_api';
 
 const metricsObs = params => {
+  const timeSlider =
+    params.timeSlider === 'auto'
+      ? config.metrics.expiration
+      : params.timeSlider;
   const timeFilter = pickKeys(['start', 'end'])({
     ...{
-      start: new Date().getTime() - params.timeSlider * 60000,
+      start: new Date().getTime() - timeSlider * 60000,
     },
     ...params,
   });
