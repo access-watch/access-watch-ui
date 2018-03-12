ba7a4c201ed57b4a339c1f202dbd770c1ccd4b39
jduthon <duthon.jean@gmail.com>
2018-03-12 14:26:49 +0100

    Fix: ES expiration should overwrite metrics expiration now

--- src/utilities/config.js
+++ src/utilities/config.js
@@ -2,7 +2,8 @@ import config from '../app_config';
 
 export const hasElasticSearch = () => config.modules.elasticsearch.active;
 export const hasSessionTimerangeSupport = () => config.session.timerange;
-const elasticsearchOverwrites = type => ['session'].indexOf(type) !== -1;
+const elasticsearchOverwrites = type =>
+  ['session', 'metrics'].indexOf(type) !== -1;
 export const getExpiration = type => {
   if (hasElasticSearch() && elasticsearchOverwrites(type)) {
     return config.elasticsearch.expiration * 24 * 3600;
